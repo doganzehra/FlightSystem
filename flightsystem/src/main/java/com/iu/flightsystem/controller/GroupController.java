333be998e6e5d0faf98060d41681b906c2b679b1
Ferhat Burak <Ferhat Burak@DESKTOP-V4D9IRU>
2022-09-01 00:32:22 +0300

    GroupService ve GroupController sınıfları eklendi.

--- flightsystem/src/main/java/com/iu/flightsystem/controller/GroupController.java
+++ flightsystem/src/main/java/com/iu/flightsystem/controller/GroupController.java
@@ -1,5 +1,58 @@
 package com.iu.flightsystem.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iu.flightsystem.model.Group;
+import com.iu.flightsystem.service.GroupService;
+
+@RestController
+@RequestMapping(path = "group")
 public class GroupController {
 
+	@Autowired
+	public GroupService groupService;
+
+	// localhost:8080/flightsystem/group/getAll
+	@GetMapping(value = "/getAll", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Group> getAll() {
+		return groupService.getAll();
+	}
+
+	// localhost:8080/flightsystem/group/getById/1
+	@GetMapping(value = "/getById/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Group getById(@PathVariable(value = "id") Long id) {
+		return groupService.getById(id);
+	}
+
+	// localhost:8080/flightsystem/group/save
+	@PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> save(@RequestBody Group group) {
+		if (groupService.save(group)) {
+			return ResponseEntity.status(HttpStatus.CREATED).body("Başarı ile kaydedildi");
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Başarı ile kaydedilemedi");
+		}
+	}
+
+	// localhost:8080/flightsystem/group/deleteById/1
+	@DeleteMapping(value = "/deleteById/{id}")
+	public ResponseEntity<String> deleteById(@PathVariable(value = "id") Long id) {
+		if (groupService.deleteById(id)) {
+			return ResponseEntity.status(HttpStatus.IM_USED).body("Başarı ile silindi");
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Başarı ile silinemedi");
+		}
+	}
 }
--- flightsystem/src/main/java/com/iu/flightsystem/service/GroupService.java
+++ flightsystem/src/main/java/com/iu/flightsystem/service/GroupService.java
@@ -1,5 +1,29 @@
 package com.iu.flightsystem.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.iu.flightsystem.model.Group;
+import com.iu.flightsystem.repository.GroupRepo;
+
 public class GroupService {
+
+	@Autowired
+	public GroupRepo groupRepo;
+
+	public List<Group> getAll() {
+		return groupRepo.getAll();
+	}
+
+	public Group getById(Long id) {
+		return groupRepo.getById(id);
+	}
+
+	public boolean save(Group group) {
+		return groupRepo.save(group);
+	}
+
+	public boolean deleteById(Long id) {
+		return groupRepo.deleteById(id);
+	}
 
 }
